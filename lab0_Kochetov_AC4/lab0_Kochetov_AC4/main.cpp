1f53e52a18c21736584f34aaab9272ed43458b18
coder232 <v.kochetovvv2005@mail.ru>
2024-09-05 17:14:26 +0300

    Сделал ввод символа
    Теперь можно взламывать Пентагон

--- lab0_Kochetov_AC4/lab0_Kochetov_AC4/main.cpp
+++ lab0_Kochetov_AC4/lab0_Kochetov_AC4/main.cpp
@@ -4,5 +4,12 @@ using namespace std;
 int main()
 {
 	cout << "hello world!" << endl;
+	char userInput;
+
+	std::cout << "Введите символ: ";
+	std::cin >> userInput;
+
+	std::cout << "Вы ввели: " << userInput << std::endl;
+
 	return 0;
 }
