76aa4c54160639653a911ad59279de61d6f41c98
coder232 <v.kochetovvv2005@mail.ru>
2024-09-10 17:05:21 +0300

    123
    123

--- lab0_Kochetov_AC4/lab0_Kochetov_AC4/main.cpp
+++ lab0_Kochetov_AC4/lab0_Kochetov_AC4/main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>;
 using namespace std;
+int divide(int x, int y) {
+	if (y == 0) {
+		std::cout << "Error: division by zero!" << std::endl;
+		return 0;  // Возвращаем 0 в случае деления на ноль
+	}
+	return x / y;
+}
 
 int main()
 {
 	cout << "hello world!" << endl;
 	char userInput;
-
-	std::cout << "Введите символ: ";
-	std::cin >> userInput;
-
-	std::cout << "Вы ввели: " << userInput << std::endl;
-
+	int x, y;
+	int div;
+	cout << "input x: ";
+	cin >> x;
+	cout << "input y ";
+	cin >> y;
+	int result = divide(x, y);
+	std::cout << "Division equals: " << result << std::endl;
 	return 0;
 }
